1cf096a74de401336cb9d7c7b85e5a90e877d6bd
uros <uroskrupnikovic@yahoo.com>
2022-05-06 19:17:41 +0200

    Added drawer

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,23 @@
 import { init } from 'ityped'
-import {useRef, useEffect} from 'react'
-import {AiOutlineMenu} from 'react-icons'
+import {useState, useRef, useEffect} from 'react'
+import {AiOutlineMenu} from 'react-icons/ai'
+import { SwipeableDrawer } from '@mui/material';
 
 function Header() {
 
+    const [drawer, setDrawer] = useState(false);
     const logoRef = useRef();
 
     useEffect(()=>{
-        init(logoRef.current, {showCursor: false, strings: ['Uroš Krupniković', 'Krupniković Uroš'], loop: false})
+        init(logoRef.current, {showCursor: false, strings: ['Uroš Krupniković', 'Крупниковић Урош'], loop: false})
     }, []);
 
     return ( <header>
         <code ref={logoRef}></code>
-
+        <AiOutlineMenu onClick={()=>{setDrawer(true)}} className='manuIcon'></AiOutlineMenu>
+        <SwipeableDrawer anchor='right' open={drawer} onClose={()=>{setDrawer(false)}}>
+            hello
+        </SwipeableDrawer>
     </header> );
 }
 
