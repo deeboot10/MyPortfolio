b24d776d7e45e57935e89ecab985e0e3c10cbbb4
uros <uroskrupnikovic@yahoo.com>
2022-05-06 15:51:32 +0200

    Added typing logo to header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,18 @@
+import { init } from 'ityped'
+import {useRef, useEffect} from 'react'
+import {AiOutlineMenu} from 'react-icons'
+
 function Header() {
+
+    const logoRef = useRef();
+
+    useEffect(()=>{
+        init(logoRef.current, {showCursor: false, strings: ['Uroš Krupniković', 'Krupniković Uroš'], loop: false})
+    }, []);
+
     return ( <header>
-        Header
+        <code ref={logoRef}></code>
+
     </header> );
 }
 
